97d6c3577adab6d01c6ff5d966963a15091e4d7f
George Roberts <george.e.roberts@gmail.com>
2019-04-17 10:22:41 +0100

    dynamically generate pagination container

--- js/script.js
+++ js/script.js
@@ -4,17 +4,6 @@ FSJS project 2 - List Filter and Pagination
 ******************************************/
    
 
-/*** 
-   Add your global variables that store the DOM elements you will 
-   need to reference and/or manipulate. 
-   
-   But be mindful of which variables should be global and which 
-   should be locally scoped to one of the two main functions you're 
-   going to create. A good general rule of thumb is if the variable 
-   will only be used inside of a function, then it can be locally 
-   scoped to that function.
-***/
-
 const list = document.querySelector('.student-list').children;
 const perPage = 10;
 
@@ -23,10 +12,10 @@ const perPage = 10;
    Create the `showPage` function to hide all of the items in the 
    list except for the ten you want to show.
 ***/
-function showPage(list, section) {
+function showPage(list, section) { // do I need to pass in list, if it's a globally declared const?
   
-    const startIndex = perPage * section;
-    const endIndex = perPage * section + perPage - 1;
+    const startIndex = perPage * section - perPage;
+    const endIndex = perPage * section - 1;
 
     for (let i = 0; i < list.length; i++) {
         if (i >= startIndex && i <= endIndex) {
@@ -37,19 +26,32 @@ function showPage(list, section) {
     }
 }
 
-showPage(list, 0);
-
-
+showPage(list, 1);
 
 
 /*** 
    Create the `appendPageLinks function` to generate, append, and add 
    functionality to the pagination buttons.
 ***/
-function appendPageLinks() {
 
+function appendPageLinks(list, ) {
+    const sections = Math.ceil(list.length / perPage); // find out number of sections, = number of buttons
+    
+    const div = document.createElement('div');
+    const ul = document.createElement('ul');
+    const page = document.querySelector('.page');
+    div.appendChild(ul);
+    page.appendChild(div);
+
+    for () {
+        // while i <= sections create buttons
+        // each iteration, generate and append the button to the paginationUL
+    }
+    // generate buttons
+    // append buttons
+    // add functionality
 }
-
+appendPageLinks(list);
 
 
 
