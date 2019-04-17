7f36e577cc097ea633c9311c5a41eeea573b3809
George Roberts <george.e.roberts@gmail.com>
2019-04-17 09:02:14 +0100

    declare global variables and start showPage func.

--- js/script.js
+++ js/script.js
@@ -3,8 +3,6 @@ Treehouse Techdegree:
 FSJS project 2 - List Filter and Pagination
 ******************************************/
    
-// Study guide for this project - https://drive.google.com/file/d/1OD1diUsTMdpfMDv677TfL1xO2CEkykSz/view?usp=sharing
-
 
 /*** 
    Add your global variables that store the DOM elements you will 
@@ -17,23 +15,20 @@ FSJS project 2 - List Filter and Pagination
    scoped to that function.
 ***/
 
-
+const list = document.querySelector('.student-list').children;
+const perPage = 10;
 
 
 /*** 
    Create the `showPage` function to hide all of the items in the 
    list except for the ten you want to show.
-
-   Pro Tips: 
-     - Keep in mind that with a list of 54 students, the last page 
-       will only display four.
-     - Remember that the first student has an index of 0.
-     - Remember that a function `parameter` goes in the parens when 
-       you initially define the function, and it acts as a variable 
-       or a placeholder to represent the actual function `argument` 
-       that will be passed into the parens later when you call or 
-       "invoke" the function 
 ***/
+function showPage(list, section) {
+    for (let i = 0; i < list.length; i++) {
+        if (i >= startingIndex && )
+    }
+}
+
 
 
 
@@ -42,9 +37,10 @@ FSJS project 2 - List Filter and Pagination
    Create the `appendPageLinks function` to generate, append, and add 
    functionality to the pagination buttons.
 ***/
+function appendPageLinks() {
+
+}
 
 
 
 
-
-// Remember to delete the comments that came with this file, and replace them with your own code comments.
